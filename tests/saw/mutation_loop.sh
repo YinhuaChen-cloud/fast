ed6c004d039fbaa8d9d47b25c3b4b1734b1a399d
YinhuaChen <11612127@mail.sustech.edu.cn>
2023-06-09 20:54:52 +0800

    redirect stderr of saw script verification to tmp/saw_log
    delete @{MAKE} $(LOGS) in tests/saw/Makefile since they are in
    mutation_loop.sh

--- tests/saw/mutation_loop.sh
+++ tests/saw/mutation_loop.sh
@@ -13,19 +13,20 @@ cp bitcode/all_llvm.bc bitcode/all_llvm.bc.backup
 while [ "$(grep "Proof failed" tmp/saw_log)" ]
 do
 # 1. 代码突变
-  $LLVM_DIR/bin/opt -load ./libInjectFuncCall.so -legacy-inject-func-call bitcode/all_llvm.bc.backup -o bitcode/all_llvm.bc.$count
+  # $LLVM_DIR/bin/opt -load ./libInjectFuncCall.so -legacy-inject-func-call bitcode/all_llvm.bc.backup -o bitcode/all_llvm.bc.$count
+  cp bitcode/all_llvm.bc bitcode/all_llvm.bc.$count
 # 2. 对突变体 进行 SAW 验证，SAW验证时的输出放在 tmp 文件夹下
   # 由于 SAW 脚本只会对 bitcode/all_llvm.bc 进行验证，所以要用突变体覆盖 all_llvm.bc
   cp bitcode/all_llvm.bc.$count bitcode/all_llvm.bc
   # 清空 saw_log
   echo "" > tmp/saw_log
   # 运行 SAW 验证程序
-	sudo docker run -v $(pwd):/a ghcr.io/galoisinc/saw:nightly a/verify_cork_uncork.saw >> tmp/saw_log &
-	sudo docker run -v $(pwd):/a ghcr.io/galoisinc/saw:nightly a/verify_drbg.saw >> tmp/saw_log &
-	sudo docker run -v $(pwd):/a ghcr.io/galoisinc/saw:nightly a/verify_handshake.saw >> tmp/saw_log &
-	sudo docker run -v $(pwd):/a ghcr.io/galoisinc/saw:nightly a/verify_HMAC.saw >> tmp/saw_log &
-	sudo docker run -v $(pwd):/a ghcr.io/galoisinc/saw:nightly a/verify_imperative_cryptol_spec.saw >> tmp/saw_log &
-	sudo docker run -v $(pwd):/a ghcr.io/galoisinc/saw:nightly a/verify_state_machine.saw >> tmp/saw_log &
+	sudo docker run -v $(pwd):/a ghcr.io/galoisinc/saw:nightly a/verify_cork_uncork.saw 2>&1 >> tmp/saw_log &
+	sudo docker run -v $(pwd):/a ghcr.io/galoisinc/saw:nightly a/verify_drbg.saw 2>&1 >> tmp/saw_log &
+	sudo docker run -v $(pwd):/a ghcr.io/galoisinc/saw:nightly a/verify_handshake.saw 2>&1 >> tmp/saw_log &
+	sudo docker run -v $(pwd):/a ghcr.io/galoisinc/saw:nightly a/verify_HMAC.saw 2>&1 >> tmp/saw_log &
+	sudo docker run -v $(pwd):/a ghcr.io/galoisinc/saw:nightly a/verify_imperative_cryptol_spec.saw 2>&1 >> tmp/saw_log &
+	sudo docker run -v $(pwd):/a ghcr.io/galoisinc/saw:nightly a/verify_state_machine.saw 2>&1 >> tmp/saw_log &
   echo "Count is $count"
   count=$((count+1))
   # 等待SAW验证结束
